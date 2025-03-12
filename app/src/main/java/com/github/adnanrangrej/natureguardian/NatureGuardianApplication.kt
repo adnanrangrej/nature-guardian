f32b609ec33f95f8ea0d073192f4fb8526ec0e5d
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-03-12 23:29:28 +0530

    added application class for dagger-hilt setup

---
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/NatureGuardianApplication.kt
@@ -0,0 +1,8 @@
+package com.github.adnanrangrej.natureguardian
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NatureGuardianApplication : Application() {
+}
