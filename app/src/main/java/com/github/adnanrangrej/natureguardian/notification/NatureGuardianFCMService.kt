0c7a00a1d7fd5953755bed85ee07a726fbbbf514
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-03-13 12:12:21 +0530

    implemented device register code

--- app/src/main/java/com/github/adnanrangrej/natureguardian/notification/NatureGuardianFCMService.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/notification/NatureGuardianFCMService.kt
@@ -1,17 +1,36 @@
 package com.github.adnanrangrej.natureguardian.notification
 
 import android.util.Log
+import com.github.adnanrangrej.natureguardian.notification.model.TokenRequest
+import com.github.adnanrangrej.natureguardian.notification.network.BackendApiService
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class NatureGuardianFCMService : FirebaseMessagingService() {
+
+    @Inject
+    lateinit var backendApiService: BackendApiService
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Log.d("NatureGuardianFCMService", "New token: $token")
+        val tokenRequest = TokenRequest(token = token)
 
         // send token to backend to register the device with sns
-        sendTokenToBackend(token)
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = backendApiService.registerDevice(tokenRequest)
+                Log.d("NatureGuardianFCMService", "Response message: ${response.message}")
+            } catch (e: Exception) {
+                Log.e("NatureGuardianFCMService", "Failed to register device: ${e.message}")
+            }
+        }
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
@@ -27,14 +46,10 @@ class NatureGuardianFCMService : FirebaseMessagingService() {
         // Show notification to user
         showNotification(title, body)
 
-
     }
 
     private fun showNotification(title: String, body: String) {
 
-    }
-
-    private fun sendTokenToBackend(token: String) {
 
     }
 }
