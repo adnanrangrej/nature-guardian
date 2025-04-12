1c531eceb82628329942bd2f535d5b803d5279f9
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-12 10:34:08 +0530

    feat: add species repository module
    - Added `SpeciesRepository` and `SpeciesRepositoryImpl` to manage species data.
    - Created a `RepositoryModule` to provide `SpeciesRepository` using Dagger Hilt.
    - Included `FullSpeciesDetailDao` as a dependency for `SpeciesRepositoryImpl`.

--- app/src/main/java/com/github/adnanrangrej/natureguardian/di/RepositoryModule.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/di/RepositoryModule.kt
@@ -1,8 +1,11 @@
 package com.github.adnanrangrej.natureguardian.di
 
+import com.github.adnanrangrej.natureguardian.data.local.dao.species.FullSpeciesDetailDao
 import com.github.adnanrangrej.natureguardian.data.remote.api.news.NewsApiService
 import com.github.adnanrangrej.natureguardian.data.repository.NewsRepositoryImpl
+import com.github.adnanrangrej.natureguardian.data.repository.SpeciesRepositoryImpl
 import com.github.adnanrangrej.natureguardian.domain.repository.NewsRepository
+import com.github.adnanrangrej.natureguardian.domain.repository.SpeciesRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,4 +23,9 @@ object RepositoryModule {
         return NewsRepositoryImpl(newsApiService)
     }
 
+    @Provides
+    @Singleton
+    fun provideSpeciesRepository(fullSpeciesDetailDao: FullSpeciesDetailDao): SpeciesRepository {
+        return SpeciesRepositoryImpl(fullSpeciesDetailDao)
+    }
 }
