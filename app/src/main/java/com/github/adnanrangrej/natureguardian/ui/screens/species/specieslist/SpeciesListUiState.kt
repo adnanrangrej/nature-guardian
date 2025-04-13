47b5ea836a39b83f0697b0f351428a917b21e59e
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-13 15:30:41 +0530

    feat: update species list UI state to use Species model

--- app/src/main/java/com/github/adnanrangrej/natureguardian/ui/screens/species/specieslist/SpeciesListUiState.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/ui/screens/species/specieslist/SpeciesListUiState.kt
@@ -1,9 +1,9 @@
 package com.github.adnanrangrej.natureguardian.ui.screens.species.specieslist
 
-import com.github.adnanrangrej.natureguardian.domain.model.species.DetailedSpecies
+import com.github.adnanrangrej.natureguardian.domain.model.species.Species
 
 sealed interface SpeciesListUiState {
     object Loading : SpeciesListUiState
-    data class Success(val speciesList: List<DetailedSpecies> = listOf()) : SpeciesListUiState
+    data class Success(val speciesList: List<Species> = listOf()) : SpeciesListUiState
     data class Error(val message: String? = null) : SpeciesListUiState
 }
