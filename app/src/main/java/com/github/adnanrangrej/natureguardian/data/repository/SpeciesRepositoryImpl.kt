3694aa4b91f51f29bbf663b61caa975a880bc495
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-14 16:25:44 +0530

    refactor: update `SpeciesRepository` to use `DetailedSpecies`
    - Modified `getAllSpecies` and `getBookmarkedSpecies` in `SpeciesRepository` and `SpeciesRepositoryImpl` to return `List<DetailedSpecies>` instead of `List<Species>`.
    - Removed unnecessary `toSpecies` mapper.
    - Modified `SpeciesRepositoryImpl` to directly use `fullSpeciesDetailDao` methods to get list of detailed species.

--- app/src/main/java/com/github/adnanrangrej/natureguardian/data/repository/SpeciesRepositoryImpl.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/data/repository/SpeciesRepositoryImpl.kt
@@ -3,9 +3,7 @@ package com.github.adnanrangrej.natureguardian.data.repository
 import com.github.adnanrangrej.natureguardian.data.local.dao.species.FullSpeciesDetailDao
 import com.github.adnanrangrej.natureguardian.data.local.dao.species.SpeciesDao
 import com.github.adnanrangrej.natureguardian.data.mapper.toDetailedSpecies
-import com.github.adnanrangrej.natureguardian.data.mapper.toSpecies
 import com.github.adnanrangrej.natureguardian.domain.model.species.DetailedSpecies
-import com.github.adnanrangrej.natureguardian.domain.model.species.Species
 import com.github.adnanrangrej.natureguardian.domain.repository.SpeciesRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -18,13 +16,13 @@ class SpeciesRepositoryImpl(
         fullSpeciesDetailDao.getFullSpeciesDetailById(internalTaxonId)
             .map { it?.toDetailedSpecies() }
 
-    override fun getAllSpecies(): Flow<List<Species>> =
-        speciesDao.getAllSpecies()
-            .map { list -> list.map { it.toSpecies() } }
+    override fun getAllSpecies(): Flow<List<DetailedSpecies>> =
+        fullSpeciesDetailDao.getAllFullSpeciesDetails()
+            .map { list -> list.map { it.toDetailedSpecies() } }
 
-    override fun getBookmarkedSpecies(): Flow<List<Species>> =
-        speciesDao.getAllBookmarkedSpecies()
-            .map { list -> list.map { it.toSpecies() } }
+    override fun getBookmarkedSpecies(): Flow<List<DetailedSpecies>> =
+        fullSpeciesDetailDao.getAllBookmarkedSpecies()
+            .map { list -> list.map { it.toDetailedSpecies() } }
 
     override suspend fun bookmarkSpecies(
         internalTaxonId: Long,
--- app/src/main/java/com/github/adnanrangrej/natureguardian/domain/repository/SpeciesRepository.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/domain/repository/SpeciesRepository.kt
@@ -1,16 +1,15 @@
 package com.github.adnanrangrej.natureguardian.domain.repository
 
 import com.github.adnanrangrej.natureguardian.domain.model.species.DetailedSpecies
-import com.github.adnanrangrej.natureguardian.domain.model.species.Species
 import kotlinx.coroutines.flow.Flow
 
 interface SpeciesRepository {
 
     fun getSpeciesById(internalTaxonId: Long): Flow<DetailedSpecies?>
 
-    fun getAllSpecies(): Flow<List<Species>>
+    fun getAllSpecies(): Flow<List<DetailedSpecies>>
 
-    fun getBookmarkedSpecies(): Flow<List<Species>>
+    fun getBookmarkedSpecies(): Flow<List<DetailedSpecies>>
 
     suspend fun bookmarkSpecies(internalTaxonId: Long, isBookmarked: Boolean)
 }
