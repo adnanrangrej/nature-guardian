8b04daa6c937cf26a61a3415e09e798af67dea5b
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-15 21:36:50 +0530

    feat: added Species detail tab row
    - Created `SpeciesDetailTabsRow` composable for displaying tabs in the Species detail screen.
    - The composable takes a list of tab titles, a `PagerState`, and a callback for tab selection.
    - It uses a `TabRow` to display the tabs and updates the styling (color, font weight, text decoration) based on the selected tab.

---
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/ui/screens/species/speciesdetail/SpeciesDetailTabsRow.kt
@@ -0,0 +1,51 @@
+package com.github.adnanrangrej.natureguardian.ui.screens.species.speciesdetail
+
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+
+@Composable
+fun SpeciesDetailTabsRow(
+    modifier: Modifier = Modifier,
+    tabsTitle: List<String>,
+    pagerState: PagerState,
+    onTabSelected: (Int) -> Unit
+) {
+    TabRow(
+        selectedTabIndex = pagerState.currentPage,
+        modifier = modifier,
+        containerColor = Color.Transparent,
+        contentColor = MaterialTheme.colorScheme.onSurface,
+        indicator = { },
+        divider = { }
+    ) {
+        tabsTitle.forEachIndexed { index, title ->
+            val isSelected = pagerState.currentPage == index
+            val color =
+                if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurfaceVariant
+            val fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal
+            val textDecoration = if (isSelected) TextDecoration.Underline else TextDecoration.None
+
+            Tab(
+                selected = pagerState.currentPage == index,
+                onClick = { onTabSelected(index) },
+                text = {
+                    Text(
+                        text = title,
+                        color = color,
+                        style = MaterialTheme.typography.bodyLarge,
+                        textDecoration = textDecoration,
+                        fontWeight = fontWeight,
+                    )
+                }
+            )
+        }
+    }
+}
