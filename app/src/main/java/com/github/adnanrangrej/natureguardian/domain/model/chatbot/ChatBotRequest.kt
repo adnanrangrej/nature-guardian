8164642d3f29e2537881279ae8d40e169880a322
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-21 16:32:39 +0530

    refactor: remove History and Message data classes, add system instruction to ChatBotRequest
    - Removed `History` data class.
    - Removed `Message` data class.
    - Added `systemInstruction` parameter to `ChatBotRequest`.

--- app/src/main/java/com/github/adnanrangrej/natureguardian/domain/model/chatbot/ChatBotRequest.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/domain/model/chatbot/ChatBotRequest.kt
@@ -1,6 +1,7 @@
 package com.github.adnanrangrej.natureguardian.domain.model.chatbot
 
 data class ChatBotRequest(
-    val history: List<History> = listOf(),
-    val prompt: String
+    val history: List<Message> = listOf(),
+    val prompt: String,
+    val systemInstruction: String
 )
--- app/src/main/java/com/github/adnanrangrej/natureguardian/domain/model/chatbot/History.kt
+++
@@ -1,6 +0,0 @@
-package com.github.adnanrangrej.natureguardian.domain.model.chatbot
-
-data class History(
-    val content: String,
-    val role: String
-)
