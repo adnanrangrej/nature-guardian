92404479fd086d3c9ab704450e147a5c84a69b8a
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-23 23:56:57 +0530

    add firebase firestore dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.messaging)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
 
     // Retrofit
     implementation(libs.retrofit)
