866104d487b2964ea003f8457d3d7306875d4328
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-26 02:37:47 +0530

    feat: added NatureGuardian navigation graph
    Created `NatureGuardianGraph` object to define navigation graphs with constants for the root, authentication, and main graphs.

---
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/ui/navigation/destination/NatureGuardianGraph.kt
@@ -0,0 +1,7 @@
+package com.github.adnanrangrej.natureguardian.ui.navigation.destination
+
+object NatureGuardianGraph {
+    const val ROOT = "root_graph"
+    const val AUTHENTICATION = "auth_graph"
+    const val MAIN = "main_graph"
+}
