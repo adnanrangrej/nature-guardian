c6e6d5db8400024b33cef78760de58c274a72f67
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-26 02:35:30 +0530

    feat: check if user logged in on splash screen
    - Added `GetCurrentUserUseCase` dependency to `SplashScreenViewModel`.
    - Added `isLoggedIn` state to track user login status.
    - Added `checkIfUserLoggedIn` method to check current user using `GetCurrentUserUseCase`.
    - Updated `SplashScreenViewModel` to initialize `isLoggedIn` state.
    - Updated `SplashScreenViewModel` to log the login status.

--- app/src/main/java/com/github/adnanrangrej/natureguardian/ui/screens/splashscreen/SplashScreenViewModel.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/ui/screens/splashscreen/SplashScreenViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.adnanrangrej.natureguardian.data.local.preferences.PrefsHelper
+import com.github.adnanrangrej.natureguardian.domain.usecase.auth.GetCurrentUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -14,15 +15,20 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SplashScreenViewModel @Inject constructor(
-    private val prefsHelper: PrefsHelper
+    private val prefsHelper: PrefsHelper,
+    private val getCurrentUserUseCase: GetCurrentUserUseCase
 ) : ViewModel() {
 
     private val _isPrepopulated = MutableStateFlow<Boolean>(false)
     val isPrepopulated: StateFlow<Boolean> = _isPrepopulated
 
+    private val _isLoggedIn = MutableStateFlow<Boolean>(false)
+    val isLoggedIn: StateFlow<Boolean> = _isLoggedIn
+
     init {
         Log.d("SplashScreenViewModel", "SplashScreenViewModel initialized")
         prepopulateDatabase()
+        checkIfUserLoggedIn()
     }
 
     private fun prepopulateDatabase() {
@@ -38,5 +44,11 @@ class SplashScreenViewModel @Inject constructor(
                 Log.d("SplashScreenViewModel", "Prepopulated in ViewModel: ${isPrepopulated.value}")
             }
         }
+    }
+
+    private fun checkIfUserLoggedIn() {
+        val currentUser = getCurrentUserUseCase()
+        _isLoggedIn.value = currentUser != null
+        Log.d("SplashScreenViewModel", "Is User Logged In: ${_isLoggedIn.value}")
     }
 }
