31f7efb90b5f0b5e1c5e93da6fff1ee69a5168b9
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-26 02:34:38 +0530

    feat: add Firebase and authentication setup
    - Added `FirebaseModule` to provide `FirebaseAuth` and `FirebaseFirestore` instances.
    - Added `AuthRepositoryImpl` and `ProfileRepositoryImpl` to handle authentication and profile-related operations.
    - Added `AuthRepository` and `ProfileRepository` interfaces.
    - Updated `RepositoryModule` to provide `AuthRepository` and `ProfileRepository`.

--- app/src/main/java/com/github/adnanrangrej/natureguardian/di/RepositoryModule.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/di/RepositoryModule.kt
@@ -4,12 +4,18 @@ import com.github.adnanrangrej.natureguardian.data.local.dao.species.FullSpecies
 import com.github.adnanrangrej.natureguardian.data.local.dao.species.SpeciesDao
 import com.github.adnanrangrej.natureguardian.data.remote.api.chatbot.ChatBotApiService
 import com.github.adnanrangrej.natureguardian.data.remote.api.news.NewsApiService
+import com.github.adnanrangrej.natureguardian.data.repository.AuthRepositoryImpl
 import com.github.adnanrangrej.natureguardian.data.repository.ChatBotRepositoryImpl
 import com.github.adnanrangrej.natureguardian.data.repository.NewsRepositoryImpl
+import com.github.adnanrangrej.natureguardian.data.repository.ProfileRepositoryImpl
 import com.github.adnanrangrej.natureguardian.data.repository.SpeciesRepositoryImpl
+import com.github.adnanrangrej.natureguardian.domain.repository.AuthRepository
 import com.github.adnanrangrej.natureguardian.domain.repository.ChatBotRepository
 import com.github.adnanrangrej.natureguardian.domain.repository.NewsRepository
+import com.github.adnanrangrej.natureguardian.domain.repository.ProfileRepository
 import com.github.adnanrangrej.natureguardian.domain.repository.SpeciesRepository
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -40,5 +46,23 @@ object RepositoryModule {
     @Singleton
     fun provideChatBotRepository(@Named("chatbot") chatBotApiService: ChatBotApiService): ChatBotRepository {
         return ChatBotRepositoryImpl(chatBotApiService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(
+        auth: FirebaseAuth,
+        firestore: FirebaseFirestore
+    ): AuthRepository {
+        return AuthRepositoryImpl(auth, firestore)
+    }
+
+    @Provides
+    @Singleton
+    fun provideProfileRepository(
+        firestore: FirebaseFirestore,
+        auth: FirebaseAuth
+    ): ProfileRepository {
+        return ProfileRepositoryImpl(firestore, auth)
     }
 }
