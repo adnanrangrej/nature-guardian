b3593225cde4ea0c51d463c9624d204be8999043
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-26 16:22:24 +0530

    Refactor: Changed BottomNavItem to use graphRoute instead of NatureGuardianScreen
    - Modified `BottomNavItem` to store a `graphRoute` instead of a `NatureGuardianScreen`.

--- app/src/main/java/com/github/adnanrangrej/natureguardian/ui/navigation/destination/BottomNavItem.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/ui/navigation/destination/BottomNavItem.kt
@@ -3,7 +3,7 @@ package com.github.adnanrangrej.natureguardian.ui.navigation.destination
 import androidx.compose.ui.graphics.vector.ImageVector
 
 data class BottomNavItem(
-    val natureGuardianScreen: NatureGuardianScreen,
+    val graphRoute: String,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector,
     val label: String
