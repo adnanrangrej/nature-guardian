1f89a1af645a3f2e9900ef3784d86c18a37cdfc6
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-26 10:53:41 +0530

    feat: added snackbar to signup screen for error messages
    - Added a `SnackbarHostState` to `SignUpScreen` and `SignUpScreenBody` to display error messages.
    - Displayed error messages in the snackbar when they are present in the `uiState`.
    - Moved `Column` in `SignUpScreenBody` into `Box` to display snackbar at the bottom.
    - Updated `SignUpScreenBodyPreview` to pass a `SnackbarHostState` instance.

--- app/src/main/java/com/github/adnanrangrej/natureguardian/ui/screens/auth/signup/SignUpScreen.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/ui/screens/auth/signup/SignUpScreen.kt
@@ -1,9 +1,11 @@
 package com.github.adnanrangrej.natureguardian.ui.screens.auth.signup
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.github.adnanrangrej.natureguardian.ui.screens.auth.AuthViewModel
@@ -15,17 +17,25 @@ fun SignUpScreen(
     navigateToHome: () -> Unit
 ) {
     val uiState = viewModel.uiState.collectAsState()
+    val snackbarHostState = remember { SnackbarHostState() }
     SignUpScreenBody(
         modifier = Modifier.fillMaxSize(),
         onSignUpClick = viewModel::signUp,
         onLoginClick = navigateToLogin,
         onContinueAsGuestClick = {},
-        isLoading = uiState.value.isLoading
+        isLoading = uiState.value.isLoading,
+        snackbarHostState = snackbarHostState
     )
 
     LaunchedEffect(uiState.value.isAuthenticated) {
         if (uiState.value.isAuthenticated) {
             navigateToHome()
+        }
+    }
+
+    LaunchedEffect(uiState.value.errorMessage) {
+        uiState.value.errorMessage?.let {
+            snackbarHostState.showSnackbar(it)
         }
     }
 
--- app/src/main/java/com/github/adnanrangrej/natureguardian/ui/screens/auth/signup/SignUpScreenBody.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/ui/screens/auth/signup/SignUpScreenBody.kt
@@ -25,6 +25,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -48,7 +50,8 @@ fun SignUpScreenBody(
     onSignUpClick: (email: String, password: String, name: String) -> Unit,
     onLoginClick: () -> Unit,
     onContinueAsGuestClick: () -> Unit = {},
-    isLoading: Boolean
+    isLoading: Boolean,
+    snackbarHostState: SnackbarHostState
 ) {
     // State variables
     var name by remember { mutableStateOf("") }
@@ -72,199 +75,210 @@ fun SignUpScreenBody(
 
     isFormValid = isNameEntered && isEmailValid && isPasswordValid && isConfirmPasswordValid
 
-    Column(
-        modifier = modifier.padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        // Logo
-        Image(
-            painter = painterResource(R.drawable.app_icon),
-            contentDescription = "NatureGuardian Logo"
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+    Box(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Logo
+            Image(
+                painter = painterResource(R.drawable.app_icon),
+                contentDescription = "NatureGuardian Logo"
+            )
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Text(
-            text = "Sign Up",
-            style = MaterialTheme.typography.headlineLarge,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        // Name Input
-        OutlinedTextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Full Name") },
-            modifier = Modifier.fillMaxWidth(),
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Person,
-                    contentDescription = "Name"
-                )
-            },
-            enabled = !isLoading
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Sign Up",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            // Name Input
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Full Name") },
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = "Name"
+                    )
+                },
+                enabled = !isLoading
+            )
+            Spacer(modifier = Modifier.height(16.dp))
 
-        // Email Input
-        OutlinedTextField(
-            value = email,
-            onValueChange = {
-                email = it
-                isEmailValid = email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
-                emailErrorMessage =
-                    if (email.isNotEmpty() && !isEmailValid) "Invalid email format" else null
-            },
-            label = { Text("Email") },
-            isError = emailErrorMessage != null,
-            modifier = Modifier.fillMaxWidth(),
-            enabled = !isLoading,
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Mail,
-                    contentDescription = "Email Icon"
+            // Email Input
+            OutlinedTextField(
+                value = email,
+                onValueChange = {
+                    email = it
+                    isEmailValid =
+                        email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+                    emailErrorMessage =
+                        if (email.isNotEmpty() && !isEmailValid) "Invalid email format" else null
+                },
+                label = { Text("Email") },
+                isError = emailErrorMessage != null,
+                modifier = Modifier.fillMaxWidth(),
+                enabled = !isLoading,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Mail,
+                        contentDescription = "Email Icon"
+                    )
+                }
+            )
+            emailErrorMessage?.let {
+                Text(
+                    text = it, color = MaterialTheme.colorScheme.error, modifier = Modifier.align(
+                        Alignment.Start
+                    )
                 )
             }
-        )
-        emailErrorMessage?.let {
-            Text(
-                text = it, color = MaterialTheme.colorScheme.error, modifier = Modifier.align(
-                    Alignment.Start
-                )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Password Input
+            OutlinedTextField(
+                value = password,
+                onValueChange = {
+                    password = it
+                    isPasswordValid = password.isNotEmpty() && password.length >= 6
+                    passwordErrorMessage =
+                        if (password.isNotEmpty() && !isPasswordValid) "Password must be at least 6 characters" else null
+
+                    if (confirmPassword.isNotEmpty()) {
+                        isConfirmPasswordValid = it == confirmPassword
+                        confirmPasswordErrorMessage =
+                            if (!isConfirmPasswordValid) "Passwords do not match" else null
+                    }
+                },
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth(),
+                trailingIcon = {
+                    IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                        Icon(
+                            imageVector = if (isPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                            contentDescription = if (isPasswordVisible) "Hide password" else "Show password"
+                        )
+                    }
+                },
+                enabled = !isLoading,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Password Icon"
+                    )
+                },
+                isError = passwordErrorMessage != null,
+                visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
             )
-        }
-        Spacer(modifier = Modifier.height(16.dp))
+            passwordErrorMessage?.let {
+                Text(
+                    text = it, color = MaterialTheme.colorScheme.error, modifier = Modifier.align(
+                        Alignment.Start
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
 
-        // Password Input
-        OutlinedTextField(
-            value = password,
-            onValueChange = {
-                password = it
-                isPasswordValid = password.isNotEmpty() && password.length >= 6
-                passwordErrorMessage =
-                    if (password.isNotEmpty() && !isPasswordValid) "Password must be at least 6 characters" else null
-
-                if (confirmPassword.isNotEmpty()) {
-                    isConfirmPasswordValid = it == confirmPassword
+            // Confirm Password Input
+            OutlinedTextField(
+                value = confirmPassword,
+                onValueChange = {
+                    confirmPassword = it
+                    isConfirmPasswordValid =
+                        confirmPassword.isNotEmpty() && confirmPassword == password
                     confirmPasswordErrorMessage =
-                        if (!isConfirmPasswordValid) "Passwords do not match" else null
-                }
-            },
-            label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth(),
-            trailingIcon = {
-                IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                        if (confirmPassword.isNotEmpty() && !isConfirmPasswordValid) "Passwords do not match" else null
+                },
+                label = { Text("Confirm Password") },
+                modifier = Modifier.fillMaxWidth(),
+                trailingIcon = {
+                    IconButton(onClick = { isConfirmPasswordVisible = !isConfirmPasswordVisible }) {
+                        Icon(
+                            imageVector = if (isConfirmPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                            contentDescription = if (isConfirmPasswordVisible) "Hide password" else "Show password"
+                        )
+                    }
+                },
+                leadingIcon = {
                     Icon(
-                        imageVector = if (isPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
-                        contentDescription = if (isPasswordVisible) "Hide password" else "Show password"
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Password Icon"
                     )
-                }
-            },
-            enabled = !isLoading,
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Lock,
-                    contentDescription = "Password Icon"
-                )
-            },
-            isError = passwordErrorMessage != null,
-            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
-        )
-        passwordErrorMessage?.let {
-            Text(
-                text = it, color = MaterialTheme.colorScheme.error, modifier = Modifier.align(
-                    Alignment.Start
-                )
+                },
+                enabled = !isLoading,
+                isError = confirmPasswordErrorMessage != null,
+                visualTransformation = if (isConfirmPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
             )
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Confirm Password Input
-        OutlinedTextField(
-            value = confirmPassword,
-            onValueChange = {
-                confirmPassword = it
-                isConfirmPasswordValid = confirmPassword.isNotEmpty() && confirmPassword == password
-                confirmPasswordErrorMessage =
-                    if (confirmPassword.isNotEmpty() && !isConfirmPasswordValid) "Passwords do not match" else null
-            },
-            label = { Text("Confirm Password") },
-            modifier = Modifier.fillMaxWidth(),
-            trailingIcon = {
-                IconButton(onClick = { isConfirmPasswordVisible = !isConfirmPasswordVisible }) {
-                    Icon(
-                        imageVector = if (isConfirmPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
-                        contentDescription = if (isConfirmPasswordVisible) "Hide password" else "Show password"
+            confirmPasswordErrorMessage?.let {
+                Text(
+                    text = it, color = MaterialTheme.colorScheme.error, modifier = Modifier.align(
+                        Alignment.Start
                     )
-                }
-            },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Lock,
-                    contentDescription = "Password Icon"
                 )
-            },
-            enabled = !isLoading,
-            isError = confirmPasswordErrorMessage != null,
-            visualTransformation = if (isConfirmPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
-        )
-        confirmPasswordErrorMessage?.let {
-            Text(
-                text = it, color = MaterialTheme.colorScheme.error, modifier = Modifier.align(
-                    Alignment.Start
-                )
-            )
-        }
-
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Submit Button
-        Button(
-            onClick = { onSignUpClick(email, password, name) },
-            modifier = Modifier.fillMaxWidth(),
-            enabled = !isLoading && isFormValid
-        ) {
-            Box(contentAlignment = Alignment.Center) {
-                if (isLoading) {
-                    CircularProgressIndicator()
-                } else {
-                    Text("Create Account")
-                }
             }
 
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Submit Button
+            Button(
+                onClick = { onSignUpClick(email, password, name) },
+                modifier = Modifier.fillMaxWidth(),
+                enabled = !isLoading && isFormValid
+            ) {
+                Box(contentAlignment = Alignment.Center) {
+                    if (isLoading) {
+                        CircularProgressIndicator()
+                    } else {
+                        Text("Create Account")
+                    }
+                }
+
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Login Button
+            Row(
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                Text("Already have an account? ")
+                Text(
+                    text = "Login",
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier
+                        .padding(start = 4.dp)
+                        .clickable(onClick = { if (!isLoading) onLoginClick() })
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Continue as Guest Button
+            Text("or")
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = { if (!isLoading) onContinueAsGuestClick() },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                ),
+                enabled = !isLoading
+            ) {
+                Text("Continue as Guest")
+            }
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Login Button
-        Row(
-            horizontalArrangement = Arrangement.Center,
-        ) {
-            Text("Already have an account? ")
-            Text(
-                text = "Login",
-                color = MaterialTheme.colorScheme.primary,
-                modifier = Modifier
-                    .padding(start = 4.dp)
-                    .clickable(onClick = { if (!isLoading) onLoginClick() })
-            )
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Continue as Guest Button
-        Text("or")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = { if (!isLoading) onContinueAsGuestClick() },
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = MaterialTheme.colorScheme.surfaceVariant,
-                contentColor = MaterialTheme.colorScheme.onSurfaceVariant
-            ),
-            enabled = !isLoading
-        ) {
-            Text("Continue as Guest")
-        }
+        SnackbarHost(
+            hostState = snackbarHostState,
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
     }
 }
 
@@ -277,7 +291,8 @@ private fun SignUpScreenBodyPreview() {
             onSignUpClick = { _, _, _ -> },
             onLoginClick = {},
             onContinueAsGuestClick = {},
-            isLoading = false
+            isLoading = false,
+            snackbarHostState = SnackbarHostState()
         )
     }
 }
