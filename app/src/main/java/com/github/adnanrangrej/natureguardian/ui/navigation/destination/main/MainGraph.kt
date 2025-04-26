4ec3c36f7d51920116f126d9e8804fd98c6d4d3e
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-26 16:19:40 +0530

    feat: add main navigation graph destinations
    Defined `MainGraph` object to represent the main navigation destinations: `SPECIES`, `NEWS`, and `PROFILE`.

---
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/ui/navigation/destination/main/MainGraph.kt
@@ -0,0 +1,7 @@
+package com.github.adnanrangrej.natureguardian.ui.navigation.destination.main
+
+object MainGraph {
+    const val SPECIES = "species_graph"
+    const val NEWS = "news_graph"
+    const val PROFILE = "profile_graph"
+}
