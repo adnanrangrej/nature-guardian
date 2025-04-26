731e3f856016e990f3b2a4418d508c72495142db
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-26 16:20:59 +0530

    feat: add Profile navigation and screens
    - Created `ProfileNavGraph` to handle navigation related to the Profile feature.
    - Added `ProfileScreen` and `EditProfileScreen` composables for displaying user profile and editing user data, respectively.
    - Created navigation routes for `Profile` and `EditProfile` within the `ProfileNavGraph`.
    - Created `navigateToProfile` to navigate to `ProfileScreen`.
    - Added `profileScreenRoute` to add a `ProfileScreen` composable in the graph.
    - Added `editProfileScreenRoute` to add an `EditProfileScreen` composable in the graph.

---
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/ui/navigation/navgraph/main/profile/ProfileNavGraph.kt
@@ -0,0 +1,50 @@
+package com.github.adnanrangrej.natureguardian.ui.navigation.navgraph.main.profile
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.github.adnanrangrej.natureguardian.ui.navigation.destination.main.profile.ProfileNavigation
+import com.github.adnanrangrej.natureguardian.ui.screens.profile.EditProfileScreen
+import com.github.adnanrangrej.natureguardian.ui.screens.profile.ProfileScreen
+
+fun NavController.navigateToProfile() {
+    navigate(ProfileNavigation.Profile.route) {
+        popUpTo(ProfileNavigation.Profile.route) {
+            inclusive = true
+        }
+    }
+}
+
+fun NavGraphBuilder.profileNavGraph(
+    navController: NavHostController,
+    route: String,
+    onLogout: () -> Unit
+) {
+    navigation(
+        startDestination = ProfileNavigation.Profile.route,
+        route = route
+    ) {
+        profileScreenRoute(
+            onLogout = onLogout,
+            navigateToEditProfile = { navController.navigate(ProfileNavigation.EditProfile.route) }
+        )
+
+        editProfileScreenRoute(navigateToProfile = navController::navigateToProfile)
+    }
+}
+
+fun NavGraphBuilder.profileScreenRoute(onLogout: () -> Unit, navigateToEditProfile: () -> Unit) {
+    composable(route = ProfileNavigation.Profile.route) {
+        ProfileScreen(navigateToLogin = onLogout, navigateToEditProfile = navigateToEditProfile)
+    }
+}
+
+fun NavGraphBuilder.editProfileScreenRoute(navigateToProfile: () -> Unit) {
+    composable(
+        route = ProfileNavigation.EditProfile.route
+    ) {
+        EditProfileScreen(onSaveChangesClick = navigateToProfile)
+    }
+}
