171c56e8e1c899aba91023639b94386a85debbc3
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-04-26 16:19:24 +0530

    feat: add navigation helper for managing routes
    - Created a `Helper` object to manage all navigation routes.
    - Added `allScreen` list containing all `NatureGuardianNavigation` destinations.
    - Added `findScreenByRoute` function to find a screen based on the current route.
    - Updated logic to handle routes with parameters in `findScreenByRoute`.

---
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/ui/navigation/Helper.kt
@@ -0,0 +1,42 @@
+package com.github.adnanrangrej.natureguardian.ui.navigation
+
+import com.github.adnanrangrej.natureguardian.ui.navigation.destination.NatureGuardianNavigation
+import com.github.adnanrangrej.natureguardian.ui.navigation.destination.auth.AuthNavigation
+import com.github.adnanrangrej.natureguardian.ui.navigation.destination.main.news.NewsNavigation
+import com.github.adnanrangrej.natureguardian.ui.navigation.destination.main.profile.ProfileNavigation
+import com.github.adnanrangrej.natureguardian.ui.navigation.destination.main.species.SpeciesNavigation
+import com.github.adnanrangrej.natureguardian.ui.navigation.destination.splash.SplashNavigation
+
+object Helper {
+    val allScreen = listOf<NatureGuardianNavigation>(
+        // Splash
+        SplashNavigation.Splash,
+
+        // Auth
+        AuthNavigation.Login,
+        AuthNavigation.Register,
+
+        // News
+        NewsNavigation.NewsList,
+        NewsNavigation.NewsDetail,
+
+        // Profile
+        ProfileNavigation.Profile,
+        ProfileNavigation.EditProfile,
+
+        // Species
+        SpeciesNavigation.SpeciesList,
+        SpeciesNavigation.SpeciesDetail
+    )
+
+    fun findScreenByRoute(currentRoute: String?): NatureGuardianNavigation? {
+        return allScreen.find { screen ->
+            val baseRoute = screen.route.substringBefore("/{")
+            if (screen.route.contains("/{")) {
+                currentRoute?.startsWith("$baseRoute/") == true
+            } else {
+                currentRoute == screen.route
+            }
+        }
+    }
+}
