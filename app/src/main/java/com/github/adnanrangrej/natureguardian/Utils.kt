b4c8a7d387a5d253be9aeb4581cd29690c2f65b8
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-05-11 11:02:37 +0530

    feat: add Cloudinary integration
    - Added functions to retrieve Cloudinary cloud name, API key, and API secret from environment variables using `dotenv`.
    - Added a Dagger Hilt module to provide a singleton instance of `Cloudinary` configured with the retrieved credentials.

--- app/src/main/java/com/github/adnanrangrej/natureguardian/Utils.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/Utils.kt
@@ -12,5 +12,19 @@ object Utils {
     fun getBackendBaseUrl(): String {
         return dotenv["BACKEND_BASE_URL"] ?: throw Exception("Backend base URL is missing")
     }
+
+    fun getCloudinaryCloudName(): String {
+        return dotenv["CLOUDINARY_CLOUD_NAME"]
+            ?: throw Exception("Cloudinary cloud name is missing")
+    }
+
+    fun getCloudinaryApiKey(): String {
+        return dotenv["CLOUDINARY_API_KEY"] ?: throw Exception("Cloudinary API key is missing")
+    }
+
+    fun getCloudinaryApiSecret(): String {
+        return dotenv["CLOUDINARY_API_SECRET"]
+            ?: throw Exception("Cloudinary API secret is missing")
+    }
 }
 
