5bf1299d46c0d4b69cf3bac407f10816c5ae9db0
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-05-11 11:04:23 +0530

    feat: add profile image upload functionality
    Added a `uploadProfileImage` suspend function to the `ProfileRepository` interface to support uploading user profile images. This function takes a `File` object representing the image and is expected to return the URL of the uploaded image.

--- app/src/main/java/com/github/adnanrangrej/natureguardian/domain/repository/ProfileRepository.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/domain/repository/ProfileRepository.kt
@@ -3,8 +3,10 @@ package com.github.adnanrangrej.natureguardian.domain.repository
 import com.github.adnanrangrej.natureguardian.domain.model.profile.ProfileResult
 import com.github.adnanrangrej.natureguardian.domain.model.profile.User
 import kotlinx.coroutines.flow.Flow
+import java.io.File
 
 interface ProfileRepository {
     fun getUserProfile(): Flow<ProfileResult>
     fun updateUserProfile(user: User): Flow<ProfileResult>
+    suspend fun uploadProfileImage(file: File): String?
 }
