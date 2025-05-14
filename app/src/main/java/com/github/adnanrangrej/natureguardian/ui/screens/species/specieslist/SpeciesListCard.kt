df3ac157cf1b04f09d17b4b1e776a7ebbc9f2f65
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-05-14 20:15:26 +0530

    refactor: Redesign SpeciesListCard layout and add shimmer effect
    - Changed `SpeciesListCard` layout to a horizontal arrangement with the image on the left and text on the right.
    - Adjusted padding and aspect ratio for the new layout.
    - Replaced the `SuggestionChip` with a new custom `ConservationStatusChip` component for the redlist category.
    - Added a dedicated `SpeciesListCardShimmer` composable to display a shimmering loading state for the card.
    - Integrated `ConservationStatusChipShimmer` into the `SpeciesListCardShimmer`.
    - Modified the preview to reflect the updated card structure.

--- app/src/main/java/com/github/adnanrangrej/natureguardian/ui/screens/species/specieslist/SpeciesListCard.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/ui/screens/species/specieslist/SpeciesListCard.kt
@@ -10,28 +10,28 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SuggestionChip
-import androidx.compose.material3.SuggestionChipDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.adnanrangrej.natureguardian.domain.model.species.DetailedSpecies
+import com.github.adnanrangrej.natureguardian.ui.components.ConservationStatusChip
+import com.github.adnanrangrej.natureguardian.ui.components.ConservationStatusChipShimmer
 import com.github.adnanrangrej.natureguardian.ui.components.NatureGuardianImages
 import com.github.adnanrangrej.natureguardian.ui.components.shimmerEffect
 import com.github.adnanrangrej.natureguardian.ui.screens.species.getDrawableResourceId
 import com.github.adnanrangrej.natureguardian.ui.screens.species.getFakeSpecies
-import com.github.adnanrangrej.natureguardian.ui.screens.species.getStatusColor
 import com.github.adnanrangrej.natureguardian.ui.theme.NatureGuardianTheme
 import com.github.adnanrangrej.natureguardian.ui.theme.SpeciesCardShape
 
@@ -44,58 +44,58 @@ fun SpeciesListCard(
 ) {
     Card(
         modifier = modifier
-            .padding(8.dp),
+            .fillMaxWidth()
+            .aspectRatio(3f),
         shape = SpeciesCardShape,
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
-        // Species Image
-        NatureGuardianImages(
-            url = imageUrl ?: "",
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(1f)
-                .padding(16.dp)
-                .clip(RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp)),
-            placeholder = getDrawableResourceId(species.species.className),
-            contentScale = ContentScale.Crop
-        )
-
-        Spacer(Modifier.height(16.dp))
-
-
-        // Species Details
-        Column(
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Text(
-                text = commonName ?: species.species.scientificName,
-                style = MaterialTheme.typography.titleLarge,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-            )
-            Text(
-                text = species.species.scientificName,
-                style = MaterialTheme.typography.labelLarge,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-            )
-
-            Row {
-                SuggestionChip(
-                    onClick = {},
-                    label = {
-                        Text(
-                            text = species.species.redlistCategory,
-                            style = MaterialTheme.typography.labelLarge
-                        )
-                    },
-                    shape = RoundedCornerShape(8.dp),
-                    colors = SuggestionChipDefaults.suggestionChipColors(
-                        containerColor = getStatusColor(species.species.redlistCategory),
-                        labelColor = Color.White
-                    )
+        Box(modifier = Modifier.fillMaxSize()) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                // Species Image
+                NatureGuardianImages(
+                    url = imageUrl ?: "",
+                    modifier = Modifier
+                        .weight(0.3f)
+                        .aspectRatio(1f)
+                        .clip(RoundedCornerShape(12.dp))
+                        .shadow(4.dp, RoundedCornerShape(12.dp)),
+                    placeholder = getDrawableResourceId(species.species.className),
+                    contentScale = ContentScale.Crop
                 )
+
+                Spacer(Modifier.width(16.dp))
+
+
+                // Species Details
+                Column(
+                    modifier = Modifier.weight(0.7f),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Text(
+                        text = commonName ?: species.species.scientificName,
+                        style = MaterialTheme.typography.titleMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                    Text(
+                        text = species.species.scientificName,
+                        style = MaterialTheme.typography.bodyMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        ConservationStatusChip(redlistCategory = species.species.redlistCategory)
+                    }
+                }
             }
         }
     }
@@ -105,47 +105,46 @@ fun SpeciesListCard(
 fun SpeciesListCardShimmer(modifier: Modifier = Modifier) {
     Card(
         modifier = modifier
-            .padding(8.dp),
+            .fillMaxWidth()
+            .aspectRatio(3f),
         shape = SpeciesCardShape,
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
-        // Image
-        Box(
+        Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(4f / 3f)
-                .padding(16.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .shimmerEffect()
-        )
-        Spacer(Modifier.height(16.dp))
-
-        // Detail
-        Column(
-            modifier = Modifier.padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+                .fillMaxSize()
+                .padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(24.dp)
-                    .shimmerEffect()
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(24.dp)
+                    .weight(0.3f)
+                    .aspectRatio(1f)
+                    .clip(RoundedCornerShape(12.dp))
                     .shimmerEffect()
             )
 
-            Spacer(Modifier.height(16.dp))
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(24.dp)
-                    .shimmerEffect()
-            )
+            Spacer(Modifier.width(16.dp))
+
+            // Text Shimmer
+            Column(
+                modifier = Modifier.weight(0.7f),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                repeat(2) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(18.dp)
+                            .clip(RoundedCornerShape(4.dp))
+                            .shimmerEffect()
+                    )
+                }
+
+                ConservationStatusChipShimmer()
+            }
         }
+
     }
 }
 
@@ -156,18 +155,12 @@ fun SpeciesListCardPreview(modifier: Modifier = Modifier) {
     NatureGuardianTheme(
         darkTheme = false
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            SpeciesListCard(
-                modifier = modifier,
-                species = getFakeSpecies(),
-                imageUrl = "",
-                commonName = "Tiger",
-            )
-        }
+        SpeciesListCard(
+            modifier = modifier,
+            species = getFakeSpecies(),
+            imageUrl = "",
+            commonName = "Tiger",
+        )
     }
 }
 
