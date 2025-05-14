91e2edfeb13c165e8970d9117972aec46b588ee2
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-05-14 12:19:55 +0530

    refactor: retrieve configuration values from BuildConfig
    - Removed the use of `dotenv` for retrieving configuration values from an `.env` file.
    - Updated `getBackendBaseUrl()`, `getCloudinaryCloudName()`, `getCloudinaryApiKey()`, and `getCloudinaryApiSecret()` to read values directly from `BuildConfig`.
    - Renamed `getCloudinaryApiKey()` to `getCloudinaryBackendUrl()` and `getCloudinaryApiSecret()` to `getCloudinaryBackendUrlApiKey()` to reflect the new source of these values.

--- app/src/main/java/com/github/adnanrangrej/natureguardian/Utils.kt
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/Utils.kt
@@ -1,30 +1,22 @@
 package com.github.adnanrangrej.natureguardian
 
-import io.github.cdimascio.dotenv.dotenv
-
 
 object Utils {
-    val dotenv = dotenv {
-        directory = "/assets"
-        filename = "env"
-    }
 
     fun getBackendBaseUrl(): String {
-        return dotenv["BACKEND_BASE_URL"] ?: throw Exception("Backend base URL is missing")
+        return BuildConfig.BACKEND_BASE_URL
     }
 
     fun getCloudinaryCloudName(): String {
-        return dotenv["CLOUDINARY_CLOUD_NAME"]
-            ?: throw Exception("Cloudinary cloud name is missing")
+        return BuildConfig.CLOUDINARY_CLOUD_NAME
     }
 
-    fun getCloudinaryApiKey(): String {
-        return dotenv["CLOUDINARY_API_KEY"] ?: throw Exception("Cloudinary API key is missing")
+    fun getCloudinaryBackendUrl(): String {
+        return BuildConfig.CLOUDINARY_BACKEND_URL
     }
 
-    fun getCloudinaryApiSecret(): String {
-        return dotenv["CLOUDINARY_API_SECRET"]
-            ?: throw Exception("Cloudinary API secret is missing")
+    fun getCloudinaryBackendUrlApiKey(): String {
+        return BuildConfig.CLOUDINARY_BACKEND_URL_API_KEY
     }
 }
 
