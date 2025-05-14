952eb88073c11f46a1b26ccc0aeea15b2a49d4d1
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-05-15 02:25:54 +0530

    feat: Implement Distribution Map ViewModel
    - Created `DistributionMapViewModel` to manage the state and data for the distribution map screen.
    - Utilized `GetAllSpeciesUseCase` to fetch species data with location information.
    - Transformed the fetched species data into a list of `SpeciesClusterItem` objects, suitable for map clustering.
    - Exposed a `StateFlow<DistributionMapUiState>` to represent the loading, success, or error state of the UI.
    - Handled errors during data fetching and emitted an `Error` state.
    - Used `stateIn` with `SharingStarted.WhileSubscribed` to manage the flow lifecycle.

---
+++ app/src/main/java/com/github/adnanrangrej/natureguardian/ui/screens/location/DistributionMapViewModel.kt
@@ -0,0 +1,48 @@
+package com.github.adnanrangrej.natureguardian.ui.screens.location
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.adnanrangrej.natureguardian.domain.usecase.species.GetAllSpeciesUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class DistributionMapViewModel @Inject constructor(
+    private val getAllSpeciesUseCase: GetAllSpeciesUseCase
+) : ViewModel() {
+    val uiState: StateFlow<DistributionMapUiState> =
+        getAllSpeciesUseCase().map { speciesList ->
+            val clusterItems = mutableListOf<SpeciesClusterItem>()
+            speciesList.forEach { species ->
+                val speciesId = species.species.internalTaxonId
+                val scientificName = species.species.scientificName
+                species.locations.forEach { location ->
+                    val latitude = location.latitude
+                    val longitude = location.longitude
+                    clusterItems.add(
+                        SpeciesClusterItem(
+                            speciesId = speciesId,
+                            speciesName = scientificName,
+                            latitude = latitude,
+                            longitude = longitude
+                        )
+                    )
+                }
+            }
+            DistributionMapUiState.Success(clusterItems = clusterItems) as DistributionMapUiState
+
+        }
+            .catch { throwable ->
+                emit(DistributionMapUiState.Error(throwable.message ?: "Unknown Error Occurred"))
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = DistributionMapUiState.Loading
+            )
+}
