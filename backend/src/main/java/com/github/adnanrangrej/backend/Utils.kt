626745c5abae10dbb700e58a0bdab0ac54799fd4
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-05-20 19:45:03 +0530

    refactor: remove Dotenv dependency for environment variable retrieval

--- backend/src/main/java/com/github/adnanrangrej/backend/Utils.kt
+++ backend/src/main/java/com/github/adnanrangrej/backend/Utils.kt
@@ -1,29 +1,29 @@
 package com.github.adnanrangrej.backend
 
-import io.github.cdimascio.dotenv.Dotenv
-
 fun getApiKey(): String {
     // Try to get the API_KEY from the system environment variables.
-    // If it's not set, load from the .env file.
-    return System.getenv("GNEWS_API_KEY") ?: Dotenv.load()["GNEWS_API_KEY"] ?: throw Exception("NEWS API_KEY is missing")
+    return System.getenv("GNEWS_API_KEY") ?: throw Exception("NEWS API_KEY is missing")
 }
 
 fun getSnsTopicArn(): String {
-    return System.getenv("SNS_TOPIC_ARN") ?: Dotenv.load()["SNS_TOPIC_ARN"] ?: throw Exception("SNS_TOPIC_ARN is missing")
+    return System.getenv("SNS_TOPIC_ARN") ?: throw Exception("SNS_TOPIC_ARN is missing")
 }
 
 fun getRegion(): String {
-    return System.getenv("AWS_REGION") ?: Dotenv.load()["AWS_REGION"] ?: "us-east-1"
+    return System.getenv("AWS_REGION") ?: throw Exception("AWS_REGION is missing")
 }
 
 fun getPlatformApplicationArn(): String {
-    return System.getenv("PLATFORM_APPLICATION_ARN") ?: Dotenv.load()["PLATFORM_APPLICATION_ARN"] ?: throw Exception("PLATFORM_APPLICATION_ARN is missing")
+    return System.getenv("PLATFORM_APPLICATION_ARN")
+        ?: throw Exception("PLATFORM_APPLICATION_ARN is missing")
 }
 
 fun getNewsArticlesTableName(): String {
-    return System.getenv("DYNAMODB_NEWSARTICLES_TABLE_NAME") ?: Dotenv.load()["DYNAMODB_NEWSARTICLES_TABLE_NAME"] ?: throw Exception("News Articles Table name is missing")
+    return System.getenv("DYNAMODB_NEWSARTICLES_TABLE_NAME")
+        ?: throw Exception("News Articles Table name is missing")
 }
 
 fun getNewsMetaDataTableName(): String {
-    return System.getenv("DYNAMODB_NEWMETADATA_TABLE_NAME") ?: Dotenv.load()["DYNAMODB_NEWMETADATA_TABLE_NAME"] ?: throw Exception("News Meta Data Table name is missing")
+    return System.getenv("DYNAMODB_NEWMETADATA_TABLE_NAME")
+        ?: throw Exception("News Meta Data Table name is missing")
 }
